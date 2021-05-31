27c48023e0015feb836d53483d9a3808e2bd4522
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-05-31 18:17:54 +0900

    Update The-Time-in-Words.cpp

--- The-Time-in-Words.cpp
+++ The-Time-in-Words.cpp
@@ -1,1 +1,38 @@
+string timeInWords(int h, int m)
+{
+    string result;
 
+    vector<string> v =
+    {   "zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+        "twenty one", "twenty two", "twenty three", "twenty four", "twenty five",
+        "twenty six", "twenty seven", "twenty eight", "twenty nine"
+    };
+    
+    if (m <= 30)
+    {
+        if (m == 0)
+            result = v[h] + " o' clock";
+        else if (m == 15)
+            result = "quarter past " + v[h];
+        else if (m == 30)
+            result = "half past " + v[h];
+        else if( m == 1)
+            result = v[m] + " minute past " + v[h];
+        else
+            result = v[m] + " minutes past " + v[h];
+    }
+    else
+    {
+        if (m == 45)
+            result = "quarter to " + v[h+1];
+        else if (m == 59)
+            result = v[60-m] + " minute to " + v[h+1];
+        else
+            result = v[60-m] + " minutes to " + v[h+1];
+     }
+ 
+    return result;
+}
