b4baba6a3261b7aff7cfd4c590178d907483ea14
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-05-10 10:26:53 +0900

    Update The-Time-in-Words.java

--- The-Time-in-Words.java
+++ The-Time-in-Words.java
@@ -62,3 +62,21 @@ class Result {
 
 }
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
